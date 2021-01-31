2fb2c3c113d2c8d10df2f34f524d2ab6038dc6d9
Luciano G <luciano.grandi.m@gmail.com>
2021-01-31 18:06:11 -0300

    FIX:Responsive table seguimiento

--- resources/views/includes/mdl_sent_mails.blade.php
+++ resources/views/includes/mdl_sent_mails.blade.php
@@ -5,53 +5,55 @@
     </button>
   </div>
   <div class="modal-body">
-    <table class="table table-sm">
-      <thead class="thead-light">
-        <tr>
-          <th scope="col">Destinatario</th>
-          <th scope="col">Mail</th>
-          <th scope="col">Tipo</th>
-          <th scope="col">Estado</th>
-          <th scope="col">Fecha lectura</th>
-        </tr>
-      </thead>
-      <tbody>
-            @foreach ($correos as $correo)
-                <tr>
-                    <td>{{$correo["name"]}}</td>
-                    <td>{{$correo["email"]}}</td>
-                    <td>
-                      @if ($correo["team"] == "ALUMNO")
-                        <span class="badge badge-primary">{{$correo["team"]}}</span> 
-                      @else
-                        <span class="badge badge-warning">{{$correo["team"]}}</span> 
-                      @endif
-                      
-                    </td>
-                    <td>
-                      @if ($correo["send_status"] == "CARGANDO")
-                        <span class="badge badge-secondary">En cola</span>
-                      @elseif($correo["send_status"] == "ENVIADO")
-                        <span class="badge badge-success">Enviado</span>
-                      @endif
-                    </td>
-                    <td>
-                        @if (isset($correo["date_mail_readed"]))
-                            {{$correo["date_mail_readed"]}}
-                        @else
-                            Pendiente.
-                        @endif
-                    </td>
-                    {{-- <td>
-                        <div class="custom-control custom-switch">
-                            <input type="checkbox" class="custom-control-input" id="customSwitch1">
-                            <label class="custom-control-label" for="customSwitch1">Leído</label>
-                        </div>
-                    </td>                             --}}
-                </tr>    
-            @endforeach
-      </tbody>
-    </table>
+    <div class="table-responsive">
+        <table class="table table-sm">
+          <thead class="thead-light">
+            <tr>
+              <th scope="col">Destinatario</th>
+              <th scope="col">Mail</th>
+              <th scope="col">Tipo</th>
+              <th scope="col">Estado</th>
+              <th scope="col">Fecha lectura</th>
+            </tr>
+          </thead>
+          <tbody>
+                @foreach ($correos as $correo)
+                    <tr>
+                        <td>{{$correo["name"]}}</td>
+                        <td>{{$correo["email"]}}</td>
+                        <td>
+                          @if ($correo["team"] == "ALUMNO")
+                            <span class="badge badge-primary">{{$correo["team"]}}</span> 
+                          @else
+                            <span class="badge badge-warning">{{$correo["team"]}}</span> 
+                          @endif
+                          
+                        </td>
+                        <td>
+                          @if ($correo["send_status"] == "CARGANDO")
+                            <span class="badge badge-secondary">En cola</span>
+                          @elseif($correo["send_status"] == "ENVIADO")
+                            <span class="badge badge-success">Enviado</span>
+                          @endif
+                        </td>
+                        <td>
+                            @if (isset($correo["date_mail_readed"]))
+                                {{$correo["date_mail_readed"]}}
+                            @else
+                                Pendiente.
+                            @endif
+                        </td>
+                        {{-- <td>
+                            <div class="custom-control custom-switch">
+                                <input type="checkbox" class="custom-control-input" id="customSwitch1">
+                                <label class="custom-control-label" for="customSwitch1">Leído</label>
+                            </div>
+                        </td>                             --}}
+                    </tr>    
+                @endforeach
+          </tbody>
+        </table>
+    </div>
   </div>
   <div class="modal-footer">
     <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
