30294268b5d009172f511362492c24ba4c0085cd
Kevin Delva <kdelva@retailsbs.com>
2021-10-28 12:24:39 -0300

    FIX: Modal destinatarios Title

--- resources/views/mails/send_mail.blade.php
+++ resources/views/mails/send_mail.blade.php
@@ -99,7 +99,7 @@ Enviar correo
                 <div class="modal-dialog modal-xl" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Buscar Destinatarios</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
