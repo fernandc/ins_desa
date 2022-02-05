6dbf55b4651ccdce4f5a9433d5c465c128f3331c
Fernando DC <fernando.dc.dex@gmail.com>
2022-02-05 10:44:42 -0300

    RMV: Log Message

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        Log::debug("Running Scheduled");
         $schedule->command('schedulerSendMail')->everyFiveMinutes();
     }
 
