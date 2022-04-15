5fbbfced880be6f385375356524897faf7c12ec7
Fernando DC <fernando.dc.dex@gmail.com>
2022-04-15 12:31:30 -0400

    MOD: Include de seccion de privilegios

--- resources/views/adm_users.blade.php
+++ resources/views/adm_users.blade.php
@@ -124,77 +124,7 @@ Administrar Usuarios
                     </table>
                 </div>
             @elseif(isset($_GET["tab"]) && $_GET["tab"] == "2")
-                <div class="tab-pane fade show active" id="privileges" role="tabpanel" aria-labelledby="privileges-tab">
-                    <div class="table-responsive">
-                        <table class="table " style="text-align: center;" id="lista_staff">
-                            <thead class="thead-light">
-                                <tr>
-                                    <th scope="col">Rut</th>
-                                    <th scope="col">Nombre Completo</th>
-                                    <th scope="col">Email Institucional</th>
-                                    <th scope="col">Administrador</th>
-                                    <th scope="col">Estado</th>
-                                    <th scope="col">Privilegios</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($staff as $row)
-                                    <tr>
-                                        <td>{{$row["rut"]}}</td>
-                                        <td>{{$row["nombres"]}} {{$row["apellido_paterno"]}} {{$row["apellido_materno"]}}</td>
-                                        <td>{{$row["email_institucional"]}}</td>
-                                        <td>
-                                            @if($row["administrador"]=="YES")
-                                                <a href="change_staff_admin?dni={{$row["rut"]}}" class="btn btn-primary btn-sm text-white" style="width: 45px">Si</a>
-                                            @else   
-                                                <a href="change_staff_admin?dni={{$row["rut"]}}" class="btn btn-secondary btn-sm text-white" style="width: 45px">No</a>
-                                            @endif
-                                        </td>
-                                        <td>
-                                            @if($row["estado"] == 1)
-                                                <a href="change_staff_status?dni={{$row["rut"]}}" class="btn btn-primary btn-sm">Activado</a>    
-                                            @else
-                                                <a href="change_staff_status?dni={{$row["rut"]}}" class="btn btn-secondary btn-sm">Desactivado</a>
-                                            @endif
-                                        </td>
-                                        <td><button class="btn btn-outline-primary btn-sm data-priv" data="{{$row["rut"]}}" data-toggle="modal" data-target=".bd-example-modal-xl">Administrar</button></td>
-                                    </tr>                
-                                @endforeach
-                            </tbody>
-                        </table>
-                        <script>
-                            $(".data-priv").click(function(){
-                                var dni = $(this).attr('data');
-                                Swal.fire({
-                                    icon: 'info',
-                                    title: 'Cargando',
-                                    showConfirmButton: false,
-                                })
-                                $.ajax({
-                                    type: "GET",
-                                    url: "modal_privileges",
-                                    data:{
-                                        dni
-                                    },
-                                    success: function (data)
-                                    {
-                                        $("#modalContent").html(data);
-                                        Toast.fire({
-                                            icon: 'success',
-                                            title: 'Completado'
-                                        })
-                                    }
-                                });
-                            });
-                        </script>
-                        <div class="modal fade bd-example-modal-xl" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-                            <div class="modal-dialog modal-xl" >
-                                <div class="modal-content" id="modalContent">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                @include('includes.adm_users.privilegios')
             @elseif(isset($_GET["tab"]) && $_GET["tab"] == "3")
                 @include('includes.adm_users.documents')	
             @endif
